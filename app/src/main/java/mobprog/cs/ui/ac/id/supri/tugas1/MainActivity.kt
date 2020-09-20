2dfba23c9534b344a5d0153deef86ef3c9610d00
butterchoco <ahmadreyz@gmail.com>
2020-09-20 20:21:50 +0700

    update .gitignore

---
+++ app/src/main/java/mobprog/cs/ui/ac/id/supri/tugas1/MainActivity.kt
@@ -0,0 +1,12 @@
+package mobprog.cs.ui.ac.id.supri.tugas1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
