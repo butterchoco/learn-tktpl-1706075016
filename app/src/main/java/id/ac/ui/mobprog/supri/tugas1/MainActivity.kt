947978b7db87a5b7c21aa728426f2d8aac0dbf4e
butterchoco <ahmadreyz@gmail.com>
2020-09-28 05:42:36 +0700

    Update gradle and Config Maaterial Theme

--- app/src/main/java/id/ac/ui/mobprog/supri/tugas1/MainActivity.kt
+++ app/src/main/java/id/ac/ui/mobprog/supri/tugas1/MainActivity.kt
@@ -2,6 +2,7 @@ package id.ac.ui.mobprog.supri.tugas1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
