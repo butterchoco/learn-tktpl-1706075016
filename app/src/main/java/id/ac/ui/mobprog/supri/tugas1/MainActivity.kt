c872c68eaaecb5f13961ed8051b37c73e60ba620
butterchoco <ahmadreyz@gmail.com>
2020-09-28 16:43:06 +0700

    Change layout to form

--- app/src/main/java/id/ac/ui/mobprog/supri/tugas1/MainActivity.kt
+++ app/src/main/java/id/ac/ui/mobprog/supri/tugas1/MainActivity.kt
@@ -2,12 +2,21 @@ package id.ac.ui.mobprog.supri.tugas1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var name: String = ""
+    var address: String = ""
+    var phone: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        update_profile.setOnClickListener() {
+            var user: User = User(name, phone, address)
+            user.updateProfile(name_display, address_display, phone_display)
+        }
     }
 }
--- app/src/test/java/id/ac/ui/mobprog/supri/tugas1/ExampleUnitTest.kt
+++ app/src/test/java/id/ac/ui/mobprog/supri/tugas1/ExampleUnitTest.kt
@@ -4,6 +4,8 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
+import id.ac.ui.mobprog.supri.tugas1.MainActivity
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +15,9 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun returnUserWhenProfileUpdated() {
     }
 }
