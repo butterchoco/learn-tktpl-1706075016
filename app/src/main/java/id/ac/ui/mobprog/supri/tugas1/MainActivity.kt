55bf1ca50f13232005aaa55e3d7f69a1bdc7ec4f
butterchoco <ahmadreyz@gmail.com>
2020-10-05 19:50:49 +0700

    Add functional stopwatch handler

--- app/src/main/java/id/ac/ui/mobprog/supri/tugas1/MainActivity.kt
+++ app/src/main/java/id/ac/ui/mobprog/supri/tugas1/MainActivity.kt
@@ -1,16 +1,24 @@
 package id.ac.ui.mobprog.supri.tugas1
 
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private var miliseconds = 0
+    private var isStopWatchRunning = false
     private val LOG_APP = "tugas1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(LOG_APP, "onCreate")
+        runTimer()
     }
 
     override fun onStart() {
@@ -41,5 +49,46 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(LOG_APP, "onDestroy")
+    }
+
+    fun onClickStart(view: View?) {
+        isStopWatchRunning = true
+    }
+
+    fun onClickStop(view: View?) {
+        isStopWatchRunning = false
+    }
+
+    fun onClickReset(view: View?) {
+        isStopWatchRunning = false
+        miliseconds = 0
+    }
+
+    private fun runTimer() {
+        val timeView = findViewById<TextView>(R.id.time_view)
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                val milisecs = miliseconds % 100
+                val secs = (miliseconds / 100) % 60
+				val minutes = (miliseconds / 6000) % 60;
+                val hours = miliseconds / 360000;
+
+                val time = String.format(
+                    Locale.getDefault(),
+                    "%d:%02d:%02d.%02d", hours, minutes, secs, milisecs
+                )
+
+                timeView.text = time
+                if (isStopWatchRunning) {
+                    miliseconds++
+                }
+                handler.postDelayed(this, 10)
+            }
+        })
+    }
+
+    fun onClickExit(view: View?) {
+        // TODO Exit Application
     }
 }
