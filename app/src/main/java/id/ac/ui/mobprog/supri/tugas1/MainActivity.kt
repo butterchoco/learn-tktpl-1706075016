77d4ba98cee37f4eae07c43d4963a6fb28989ef4
butterchoco <ahmadreyz@gmail.com>
2020-10-05 18:41:15 +0700

    Add state log

--- app/src/main/java/id/ac/ui/mobprog/supri/tugas1/MainActivity.kt
+++ app/src/main/java/id/ac/ui/mobprog/supri/tugas1/MainActivity.kt
@@ -1,12 +1,45 @@
 package id.ac.ui.mobprog.supri.tugas1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private val LOG_APP = "tugas1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_APP, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_APP, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_APP, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_APP, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(LOG_APP, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_APP, "onDestroy")
     }
 }
